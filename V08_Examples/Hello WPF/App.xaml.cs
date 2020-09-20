9904ae8c3a3739d9e6e7db8240425b138c4f105b
Thomas Kälin <thomas.kaelin1@ost.ch>
2020-09-20 15:58:53 +0200

    WPF - V08: Beispiele ergänzt

---
+++ V08_Examples/Hello WPF/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWPF
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
